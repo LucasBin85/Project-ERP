ee851a4b1ebe6f1170495279f808ca18c0f157b4
Lucas Bin <65771280+LucasBin85@users.noreply.github.com>
2026-07-09 20:47:22 -0300

    Add invoice id to credit card payment DTO

--- app/DTOs/Financial/CreditCardPaymentDTO.php
+++ app/DTOs/Financial/CreditCardPaymentDTO.php
@@ -6,6 +6,7 @@ final readonly class CreditCardPaymentDTO
 {
     public function __construct(
         public int $creditCardId,
+        public int $creditCardInvoiceId,
         public int $bankAccountId,
         public string $paymentDate,
         public int $amountCents,
@@ -18,6 +19,7 @@ final readonly class CreditCardPaymentDTO
     {
         return new self(
             creditCardId: (int) $data['credit_card_id'],
+            creditCardInvoiceId: (int) $data['credit_card_invoice_id'],
             bankAccountId: (int) $data['bank_account_id'],
             paymentDate: (string) $data['payment_date'],
             amountCents: (int) $data['amount_cents'],
